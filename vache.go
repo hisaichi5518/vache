1b7c7f37aebe1561866919f92ba4e1831af6ff3a
hisaichi5518 <hisaichi5518@gmail.com>
2014-08-23 20:23:40 +0900

    remove CacheData type.

--- vache.go
+++ vache.go
@@ -2,22 +2,17 @@ package vache
 
 import "time"
 
-type CacheData struct {
-	Value  string
-	Expire time.Duration
-}
-
-var cache map[string]CacheData = make(map[string]CacheData)
+var cache map[string]string = make(map[string]string)
 
 func Set(key string, val string, expire time.Duration) {
-	cache[key] = CacheData{Value: val, Expire: expire}
+	cache[key] = val
 
 	time.AfterFunc(expire, func() {
-		cache[key] = CacheData{}
+		cache[key] = ""
 	})
 }
 
 func Get(key string) string {
 	cacheData := cache[key]
-	return cacheData.Value
+	return cacheData
 }
