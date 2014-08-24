9e9465c049bc0e355e9473719ce4ba5ace4bbbe8
hisaichi5518 <hisaichi5518@gmail.com>
2014-08-24 13:55:10 +0900

    fixed tests

--- vache_test.go
+++ vache_test.go
@@ -1,6 +1,7 @@
 package vache
 
 import (
+	"bytes"
 	"testing"
 	"time"
 )
@@ -10,64 +11,64 @@ func TestSetAndGet(t *testing.T) {
 	Set("key2", "val2", time.Second)
 
 	// found cache.
-	v, _ := Get("key1")
-	if v != "val1" {
+	if v, ok := Get("key1"); !ok || v != "val1" {
 		t.Fatal("not set")
 	}
-	time.Sleep(2 * time.Second)
+
+	time.Sleep(1 * time.Second)
 
 	// expired. so, not found cache.
-	v, _ = Get("key1")
-	if v != "" {
+	if _, ok := Get("key1"); ok {
+		t.Fatal("not expired!")
+	}
+	if _, ok := Get("key2"); ok {
 		t.Fatal("not expired!")
 	}
 }
 
 func TestValueIsNotFound(t *testing.T) {
-	v, _ := Get("not-found")
-	if v != "" {
+	if v, ok := Get("not-found"); v != "" || ok {
 		t.Fatal("found 'not-found-key's cache.")
 	}
 }
 
 func TestGetOrSet(t *testing.T) {
 	key := "get_or_set"
-	v, _ := Get(key)
-	if v != "" {
+	if v, ok := Get(key); v != "" || ok {
 		t.Fatal("found 'get_or_set's cache.")
 	}
 
-	value, _ := GetOrSet(key, func() (string, time.Duration) {
+	value, isGot := GetOrSet(key, func() (string, time.Duration) {
 		return "value1", time.Second
 	})
-	if value != "value1" {
+	if value != "value1" || isGot {
 		t.Fatal("not set")
 	}
 
-	value, _ = GetOrSet(key, func() (string, time.Duration) {
+	value, isGot = GetOrSet(key, func() (string, time.Duration) {
 		return "value2", time.Second
 	})
-	if value == "value2" {
+	if value == "value2" || !isGot {
 		t.Fatal("set value2")
 	}
 
-	time.Sleep(2 * time.Second)
+	time.Sleep(1 * time.Second)
 
 	// expired. so, not found cache.
-	value, _ = Get(key)
-	if value != "" {
+	if value, ok := Get(key); value != "" || ok {
 		t.Fatal("not expired")
 	}
 }
 
 func TestDelete(t *testing.T) {
-	Set("name", "hisaichi5518", 10*time.Second)
-	if v, _ := Get("name"); v == "" {
+	value, _ := GetOrSet("name", func() (string, time.Duration) {
+		return "hisaichi5518", 10 * time.Second
+	})
+	if value == "" {
 		t.Fatal("not set")
 	}
 
-	Delete("name")
-	if v, _ := Get("name"); v != "" {
+	if deletedValue, ok := Delete("name"); deletedValue == "" || !ok {
 		t.Fatal("not delete")
 	}
 }
@@ -75,25 +76,37 @@ func TestDelete(t *testing.T) {
 func TestLock(t *testing.T) {
 	Set("name", "hisaichi5518", 10*time.Second)
 
+	ch := make(chan string)
 	go func() {
-		v, _ := Get("name")
-		if v != "hisaichi5518" {
-			t.Fatal("not get")
-		}
+		Get("name")
+		ch <- "1"
 	}()
 	go func() {
-		v, _ := Get("name")
-		if v != "hisaichi5518" {
-			t.Fatal("not get")
-		}
+		Get("name")
+		ch <- "2"
 	}()
-	go Delete("name")
-	go Set("name", "hisaichi5518", 10*time.Second)
 	go func() {
-		v, _ := Get("name")
-		if v != "hisaichi5518" {
-			t.Fatal("not get")
-		}
+		Set("name", "hisaichi5518", 10*time.Second)
+		ch <- "3"
 	}()
-	time.Sleep(10 * time.Second)
+	go func() {
+		Get("name")
+		ch <- "4"
+	}()
+	go func() {
+		Delete("name")
+		ch <- "5"
+	}()
+	go func() {
+		Get("name")
+		ch <- "6"
+	}()
+
+	var buffer bytes.Buffer
+	for i := 0; i < 6; i++ {
+		buffer.WriteString(<-ch)
+	}
+	if buffer.String() != "123456" {
+		t.Fatal("not locked", buffer.String())
+	}
 }
